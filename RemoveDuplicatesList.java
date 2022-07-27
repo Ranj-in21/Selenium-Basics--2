2bc21535244675e39459267291e66ad39c321d4c
Ranj-in21 <108992272+Ranj-in21@users.noreply.github.com>
2022-07-27 14:42:14 +0530

    Add files via upload

---
+++ RemoveDuplicatesList.java
@@ -0,0 +1,32 @@
+package javaAssignment.week3.OOPS;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class RemoveDuplicatesList {
+
+	public static void main(String[] args) {
+		
+		
+		
+		String word = "We learn java basics as part of java sessions in java week1";
+		
+		String[] text = word.split(" ");
+		Set<String> sentence = new LinkedHashSet<String>();
+					
+				for (String duplicates : text) {
+				sentence.add(duplicates);
+				
+			}
+				System.out.println(sentence);
+			
+ 		
+			
+		
+		
+		
+		
+
+	}
+
+}
